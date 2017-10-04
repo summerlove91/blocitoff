9dfd88e94683798b4d44d9a277ff61df4a9f1885
summerlove91 <gkdus3040@gmail.com>
2017-10-03 21:59:39 -0700

    todo times left & basic design added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# include Faker
+include Faker
 # require 'random_data'
-#
-# 5.times do
-#     user = User.create!(
-#     first_name:   Faker::Name.first_name,
-#     last_name:    Faker::Name.last_name,
-#     email:        Faker::Internet.email,
-#     password:     Faker::Internet.password,
-#
-#     )
-#   end
-#   users = User.all
-#
-#   50.times do
-#     item = Item.create!(
-#       user:  users.sample,
-#       name:  Faker::Lorem.sentence
-#     )
-#   end
-#
-# puts "Seed finished"
-# puts "#{Item.count} items created"
-# puts "#{User.count} users created"
+
+5.times do
+   user = User.create!(
+   first_name:   Faker::Name.first_name,
+   last_name:    Faker::Name.last_name,
+   email:        Faker::Internet.email,
+   password:     Faker::Internet.password,
+   confirmed_at: Time.now
+   )
+ end
+ users = User.all
+
+ 50.times do
+   item = Item.create!(
+     user:  users.sample,
+     name:  Faker::Lorem.sentence
+   )
+ end
+
+ 30.times do
+   item = Item.create!(
+   user: users.sample,
+   name: Faker::Lorem.sentence,
+   created_at: 3.months.ago
+   )
+ end
+
+puts "Seed finished"
+puts "#{Item.count} items created"
+puts "#{User.count} users created"
